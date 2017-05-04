662a76405cc6bd08c4ba47d0bb0e5070fb99d2c0
Ciarán Murphy <ciaran.murphy338@mail.dcu.ie>
2017-05-04 22:35:03 +0100

    Last core bot functionality

--- src/reply.py
+++ src/reply.py
@@ -1,16 +1,13 @@
 import praw
-import os
 
 reddit=praw.Reddit('sandwichBot')
 
-if not os.path.isfile("meta/posts_replied_to.txt"):
-    posts_replied_to = []
-else:
-    with open("meta/posts_replied_to.txt", "r") as f:
-       posts_replied_to = f.readlines()
-       posts_replied_to = list(filter(None, posts_replied_to))
+with open("posts_replied_to.txt", "r") as f:
+	posts_replied_to = f.readlines()
+	#posts_replied_to = list(filter(None, posts_replied_to))
 
 subreddit = reddit.subreddit('sandwichsimulator')
+
 for submission in subreddit.hot(limit=2):
 	if submission.id not in posts_replied_to:
 		if "beep" in submission.title.lower():
