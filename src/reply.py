3664f1b49119a9770eff0aaf49b579c2d5ca2724
Ciarán Murphy <ciaran.murphy338@mail.dcu.ie>
2017-05-04 03:03:22 +0100

    Bot can reply to posts

--- src/reply.py
+++ src/reply.py
@@ -1,6 +1,21 @@
 import praw
-import pdb
-import re
 import os
 
 reddit=praw.Reddit('sandwichBot')
+
+if not os.path.isfile("meta/posts_replied_to.txt"):
+    posts_replied_to = []
+else:
+    with open("meta/posts_replied_to.txt", "r") as f:
+       posts_replied_to = f.readlines()
+       posts_replied_to = list(filter(None, posts_replied_to))
+
+subreddit = reddit.subreddit('sandwichsimulator')
+for submission in subreddit.hot(limit=2):
+	if submission.id not in posts_replied_to:
+		if "beep" in submission.title.lower():
+			submission.reply("Beep boop I'm a bot!")
+			posts_replied_to.append(submission.id)
+with open("posts_replied_to.txt", "w") as f:
+	for post_id in posts_replied_to:
+		f.write(post_id + "\n")
